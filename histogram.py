87a10ab2bc76524b60ddd6b3ea62f178c56021b2
rexos <alex.pellegrini@live.com>
2013-09-19 12:11:03 +0200

    removed unused code

--- histogram.py
+++ histogram.py
@@ -1,8 +1,6 @@
 import scipy.stats as ss
 import numpy as np
 import matplotlib.pyplot as plt
-alpha, loc, beta=2, 10, 22
-x = np.linspace(50,200, 500)
 data = ss.norm.rvs(loc=0,scale=2,size=10000, facecolor='b')
 data = data + 10
 plt.hist(data,100, normed=True)
