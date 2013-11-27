f460b32b0dabeb55db36c15cdd5934b7badee83e
rexos <alex.pellegrini@live.com>
2013-11-27 22:29:29 +0100

    cleaned entropy script

--- entropy.py
+++ entropy.py
@@ -5,19 +5,28 @@ based on entropy
 from math import log
 
 def class_prob( part, total ):
+    """
+    simple probability computation
+    """
     return part / float( total )
 
 def local_entropy( classes ):
+    """
+    returns entropy value for a single cluster
+    """
     clust_size = sum( classes )
     num_class = len( classes )
-    res = float()
-    logs_data = map( lambda c: class_prob( c, clust_size), classes )
+    logs_data = map( lambda c: class_prob( c, clust_size ), classes )
     logs_data = map( lambda x: x * log( x, 2 ), logs_data )
     return -sum( logs_data ) * num_class
 
 def entropy( clusters ):
+    """
+    returns the entropy of a dict of clusters where
+    the keys could be of any type
+    """
     total_len = sum( [ len(x) for x in clusters.values() ] )
-    total = float()
-    for data in clusters.values():
-        total = total + local_entropy( data ) / total_len
-    print( total )
+    entropies = [ local_entropy( data ) /
+                  total_len for data in clusters.values() ]
+    return sum( entropies )
+
